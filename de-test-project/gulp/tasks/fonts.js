efb25926fb947ccd33b42b0131d19d0b0aa18a62
Vyacheslav Bronnikov <vierosprojumpercs@mail.ru>
2021-10-26 02:14:06 +0500

    init
    init

--- de-test-project/gulp/tasks/fonts.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    $.gulp.task('fonts', function() {
-        return $.gulp.src('src/static/fonts/*/**')
-            .pipe($.gulp.dest('build/fonts'))
-    });
-}
--- de-test-project/gulp/tasks/img.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    $.gulp.task('img', function() {
-        return $.gulp.src('src/static/img/*.{png,jpg,gif,svg}')
-            .pipe($.gulp.dest('build/img'))
-    });
-}
