accc849f536ec180bcc55b8a03f7b087b727ee04
Vyacheslav Bronnikov <vierosprojumpercs@mail.ru>
2021-11-02 02:46:35 +0500

    fixed

--- de-test-project/src/static/js/common.js
+++ de-test-project/src/static/js/common.js
@@ -21,41 +21,106 @@ document.addEventListener("DOMContentLoaded", function (event) {
             document.querySelector('html').style.overflow = null;
         }
 
+        clickHandler(btn){
+            document.querySelector(btn).addEventListener('click', () => {
+                popup.open();
+            })
+        }
     }
-    const popup = new Modal(document.querySelector('.b-popup'));
-    window.openModal = popup.open.bind(popup);
-    document.querySelector('.js-open-modal').addEventListener('click', () => {
-        window.openModal();
-    })
 
-    const form = document.querySelector('.js-form-validation')
-    const validateBtn = form.querySelector('.js-validation-btn')
-    const email = form.querySelector('.js-email')
-    const name = form.querySelector('.js-name')
-    const inputs = form.querySelectorAll('.js-input')
-    url = 'http://example.com/movies.json';
-    form.addEventListener('submit', function (event) {
-        let isValid = false;
 
-            event.preventDefault()
-            inputs.forEach(e => {
-            e.classList.remove('b-popup--error');
-        })
+    class Form {
+        constructor(form) {
+            this.form = document.querySelector(form);
+            this.url = 'https://httpbin.org/post';
+            this.onSubmit(this.form);
+            this.data = [];
+        }
 
-        for (var i = 0; i < inputs.length; i++) {
-            if (!inputs[i].value) {
-                inputs[i].classList.add('b-popup--error')
-                isValid = false;
-            } else {
-                fetch(url, {
+        //обработка сабмита
+        onSubmit(form) {
+            form.addEventListener('submit', e => {
+                e.preventDefault();
+                if (this.checkInputs(this.form)){
+                    this.sendData(this.url).then(console.log)
+                    document.querySelectorAll('.js-input').forEach(e => e.value = '');
+                    thanks.open();
+                    popup.close();
+                    this.clearClass('b-popup--done');
+                    this.clearClass('b-popup--error');
+                }
+            });
+        }
+
+        sendData(url) {
+            return fetch(url, {
                     method: 'post',
                     headers: {
-                        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+                        'Accept': 'application/json, text/plain, */*',
+                        'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify('data') // Отправка поля формы
-                })
-                .then(alert('done'))
-            }
+                    body: JSON.stringify({
+                        a: 7,
+                        str: 'Some string: &=&'
+                    })
+                }).then(res => res.status)
         }
-    })
+
+        
+        checkInputs(form) {
+            const username = form.querySelector('.js-name');
+            const email = form.querySelector('.js-email');
+
+            // чистим от пробелов
+            const usernameValue = username.value.trim();
+            const emailValue = email.value.trim();
+
+            if (usernameValue === '') {
+                this.setErrorFor(username);
+                return false;
+
+            } else {
+                this.setSuccessFor(username);
+            }
+
+            if (emailValue === '') {
+                this.setErrorFor(email);
+                return false;
+
+            } else if (!this.isEmail(emailValue)) {
+                this.setErrorFor(email);
+                return false;
+
+            } else {
+                this.setSuccessFor(email);
+            }
+
+            return true;
+        }
+
+        //Создание ошибки
+        setErrorFor(input) {
+            input.classList.add('b-popup--error');
+        }
+        //Создание обводки для верного инпута
+        setSuccessFor(input) {
+            input.classList.add('b-popup--done');
+        }
+
+        isEmail(email) {
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+        }
+
+        clearClass(className) {
+            document.querySelectorAll('.js-input').forEach(e => e.classList.remove(className));
+        }
+    }
+
+    const popup = new Modal(document.querySelector('.b-popup[data-type="popup"]'));
+    popup.clickHandler('.js-open-modal');
+    const thanks = new Modal(document.querySelector('.b-popup[data-type="thanks"]'));
+    window.openModal = popup.open.bind(popup);
+    const myform = new Form('.js-form-validation');
+    window.openModal = thanks.open.bind(thanks);
+
 });
